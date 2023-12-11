f82483fd2d2cc2f976cd195b38fdfa4b4525a11a
--global <ruoka@users.noreply.github.com>
2023-12-11 03:00:41 +0200

    Some tests

--- journal.js
+++ journal.js
@@ -1,3 +1,6 @@
+let journal = [{"header":{"reference":"A1","note":null,"file":null},"entries":[{"entry":"debit","date":"2023-12-10","account":"bank deposits","amount":11.11},{"entry":"credit","date":"2023-12-10","account":"fees","amount":11.11}]},{"header":{"reference":"A2","note":null,"file":null},"entries":[{"entry":"credit","date":"2023-12-10","account":"bank deposits","amount":22.22},{"entry":"debit","date":"2023-12-10","account":"fees","amount":11.11},{"entry":"debit","date":"2023-12-10","account":"taxes","amount":11.11}]},{"header":{"created":"2023-12-10","reference":"xxxx","note":"","file":""},"entries":[{"entry":"debit","date":"2023-12-10","account":"bank deposits","amount":"4"},{"entry":"credit","date":"2023-12-10","account":"does","amount":"2"},{"entry":"credit","date":"2023-12-10","account":"fees","amount":"1"},{"entry":"credit","date":"2023-12-10","account":"costs","amount":"1"}]}];
+
+/*
 const journal = [
     {
         header: {created: Date.now, reference: "A1", note: null, file: null},
@@ -15,3 +18,4 @@ const journal = [
         ]
     }
 ];
+*/
