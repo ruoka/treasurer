c209cfa825dbc468c332fa14ef7ca78012b488de
--global <ruoka@users.noreply.github.com>
2024-03-23 11:06:09 +0200

    Some ideas

--- accounts.js
+++ accounts.js
@@ -1,1 +1,16 @@
-export let accounts = [];
+export let accounts = {
+
+balance_sheet: {
+
+    assets: [
+        {id:1, name:"saatavat"},
+        {id:2, name:"pankkisaamiset"},
+    ],
+    equity: [],
+    liability: []
+
+},
+
+profit_and_loss: {}
+
+};
