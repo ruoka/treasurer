b890ad3d476acd4a81337b8e78fe0206586eb58b
--global <ruoka@users.noreply.github.com>
2024-03-24 22:08:36 +0200

    Refactored closer to MVC

--- general_ledger.js
+++ general_ledger.js
@@ -19,71 +19,8 @@ export let general_ledger  = {
         {id:6, name:"ostot"},
     ],
 
-isPnL: (account) => {
-    return general_ledger.revenues.find(a => a.name == account) ||
-           general_ledger.expenses.find(a => a.name == account);
-},
-
-render: (balances) => {
-
-    const populate = (tbody,name,category) => {
-
-        const group = document.createElement("optgroup");
-        group.label = name;
-
-        const tr = document.createElement("tr");
-        const th = document.createElement("th");
-        th.setAttribute("colspan","2");
-        th.textContent = name;
-        tr.appendChild(th);
-        tr.appendChild(document.createElement("th"));
-        tbody.append(tr);
-
-        category.forEach(account => {
-
-            const option = document.createElement("option");
-            option.value = account.name;
-            option.textContent = account.name;
-            group.appendChild(option);
-
-            const tr = document.createElement("tr");
-            tr.appendChild(document.createElement("td")); // Deliberately empty
-            tr.appendChild(document.createElement("td")).textContent = account.name;
-
-            if(balances.has(account.name))
-                tr.appendChild(document.createElement("td")).textContent = balances.get(account.name).toFixed(2);
-            else
-                tr.appendChild(document.createElement("td")).textContent = new Number(0.00).toFixed(2);
-
-            tbody.append(tr);
-        });
-
-        all("#account, .accounts").forEach(select => select.appendChild(group.cloneNode(true)));
-    };
-
-    all("#account, .accounts").forEach(select => select.innerHTML = "");
-
-    const tbody1 = one("table#balance tbody");
-    tbody1.innerHTML = "";
-    populate(tbody1,"assets",general_ledger.assets);
-    populate(tbody1,"net assets",general_ledger.net_assets);
-
-    const PnL = [...balances].filter(([k,v]) => general_ledger.isPnL(k)).reduce((a,[k,v]) => a+v,0);
-
-    const tr = document.createElement("tr");
-    tr.appendChild(document.createElement("td"));
-    tr.appendChild(document.createElement("td")).textContent = "PnL";
-    tr.appendChild(document.createElement("td")).textContent = new Number(PnL).toFixed(2);
-    tbody1.append(tr);
-
-    populate(tbody1,"liabilities",general_ledger.liabilities);
-    const tbody2 = one("table#PnL tbody");
-    tbody2.innerHTML = "";
-    populate(tbody2,"revenues",general_ledger.revenues);
-    populate(tbody2,"expenses",general_ledger.expenses);
-
-    one("#entry_set option[value='pankkisaamiset']").setAttribute("selected","");
-    one("#contra_entry_set option[value='maksut']").setAttribute("selected","");
-}
-
+    isPnL: (account) => {
+       return general_ledger.revenues.find(a => a.name == account) ||
+               general_ledger.expenses.find(a => a.name == account);
+    }
 };
--- treasurer.js
+++ treasurer.js
@@ -1,6 +1,6 @@
-import {one,all} from "./query.js";
-import {general_ledger} from "./general_ledger.js";
-import {journal} from "./journal.js";
+import { one, all } from "./query.js";
+import { general_ledger } from "./general_ledger.js";
+import { journal } from "./journal.js";
 
 window.onload = () => {
 
@@ -12,7 +12,7 @@ window.onload = () => {
         all("[name=date]").forEach(element => element.value = one("#date").value);
 
         const ledger = [];
-    
+
         let tbody = one("table#journal tbody");
         tbody.innerHTML = "";
 
@@ -21,7 +21,7 @@ window.onload = () => {
             entry.reference = transaction.header.reference;
             entry.note = transaction.header.note;
             ledger.push(entry);
-    
+
             const tr = document.createElement("tr");
             tr.appendChild(document.createElement("td")).textContent = entry.number;
             tr.lastChild.classList.add('reference');
@@ -34,8 +34,8 @@ window.onload = () => {
             tr.appendChild(document.createElement("td")).textContent = entry.note;
             tbody.prepend(tr);
         }));
-    
-        ledger.sort((lhs,rhs) => rhs.account.localeCompare(lhs.account) ? rhs.account.localeCompare(lhs.account) : lhs.date.localeCompare(rhs.date));
+
+        ledger.sort((lhs, rhs) => rhs.account.localeCompare(lhs.account) ? rhs.account.localeCompare(lhs.account) : lhs.date.localeCompare(rhs.date));
 
         const balances = new Map();
         let account = "";
@@ -45,13 +45,13 @@ window.onload = () => {
         tbody.innerHTML = "";
 
         ledger.forEach(entry => {
-            if(account.localeCompare(entry.account)) { // is same returns 0 == false
+            if (account.localeCompare(entry.account)) { // is same returns 0 == false
                 account = entry.account;
                 subtotal = 0.00;
             }
             subtotal += (entry.entry.localeCompare("credit") ? -1 : 1) * entry.amount;
-            balances.set(entry.account,subtotal);
-    
+            balances.set(entry.account, subtotal);
+
             const tr = document.createElement("tr");
             tr.appendChild(document.createElement("td")).textContent = entry.number;
             tr.lastChild.classList.add('reference');
@@ -64,44 +64,101 @@ window.onload = () => {
             tr.appendChild(document.createElement("td")).textContent = entry.note;
             tbody.append(tr);
         });
-    
-        general_ledger.render(balances);
 
-        all("table#ledger tbody tr, table#journal tbody tr").forEach(row => row.onclick = () => {
-            let number = row.querySelector(".reference").textContent;
-            alert(`${number}`);
+        const populate = (tbody, name, category) => {
 
-            const transaction = journal.find(trans => trans.header.number == number);
-            one("#number").value = transaction.header.number;
-            one("#created").value = transaction.header.created;
-            one("#reference").value = transaction.header.reference;
-            one("#note").value = transaction.header.note;
-            one("#file").value = transaction.header.file;
+            const group = document.createElement("optgroup");
+            group.label = name;
 
-            const divs = all("fieldset#entries div");
-            let index = 0;
-            for (let div of divs) {
-                div.children[0].value = transaction.entries[index].entry;
-                div.children[1].value = transaction.entries[index].date;
-                div.children[2].value = transaction.entries[index].account;
-                div.children[3].value = transaction.entries[index].amount.toFixed(2);
-                ++index;
-            };
-            while(index < transaction.entries.length) {
-                const div = one("#entries").appendChild(one("#contra_entry_set").cloneNode(true));
-                const button = div.appendChild(document.createElement("button"));
-                button.textContent = "Remove";
-                button.setAttribute("type", "button");
-                button.setAttribute("onclick", "this.parentElement.remove();");
+            const tr = document.createElement("tr");
+            const th = document.createElement("th");
+            th.setAttribute("colspan", "2");
+            th.textContent = name;
+            tr.appendChild(th);
+            tr.appendChild(document.createElement("th"));
+            tbody.append(tr);
 
-                div.children[0].value = transaction.entries[index].entry;
-                div.children[1].value = transaction.entries[index].date;
-                div.children[2].value = transaction.entries[index].account;
-                div.children[3].value = transaction.entries[index].amount.toFixed(2);;
-                ++index;
-            }
-        });
+            category.forEach(account => {
+
+                const option = document.createElement("option");
+                option.value = account.name;
+                option.textContent = account.name;
+                group.appendChild(option);
+
+                const tr = document.createElement("tr");
+                tr.appendChild(document.createElement("td")); // Deliberately empty
+                tr.appendChild(document.createElement("td")).textContent = account.name;
+
+                if (balances.has(account.name))
+                    tr.appendChild(document.createElement("td")).textContent = balances.get(account.name).toFixed(2);
+                else
+                    tr.appendChild(document.createElement("td")).textContent = new Number(0.00).toFixed(2);
+
+                tbody.append(tr);
+            });
+
+            all("#account, .accounts").forEach(select => select.appendChild(group.cloneNode(true)));
+        };
+
+        all("#account, .accounts").forEach(select => select.innerHTML = "");
+
+        const tbody1 = one("table#balance tbody");
+        tbody1.innerHTML = "";
+        populate(tbody1, "assets", general_ledger.assets);
+        populate(tbody1, "net assets", general_ledger.net_assets);
+
+        const PnL = [...balances].filter(([k, v]) => general_ledger.isPnL(k)).reduce((a, [k, v]) => a + v, 0);
+
+        const tr = document.createElement("tr");
+        tr.appendChild(document.createElement("td"));
+        tr.appendChild(document.createElement("td")).textContent = "PnL";
+        tr.appendChild(document.createElement("td")).textContent = new Number(PnL).toFixed(2);
+        tbody1.append(tr);
+
+        populate(tbody1, "liabilities", general_ledger.liabilities);
+        const tbody2 = one("table#PnL tbody");
+        tbody2.innerHTML = "";
+        populate(tbody2, "revenues", general_ledger.revenues);
+        populate(tbody2, "expenses", general_ledger.expenses);
+
+        one("#entry_set option[value='pankkisaamiset']").setAttribute("selected", "");
+        one("#contra_entry_set option[value='maksut']").setAttribute("selected", "");
     }
+
+    all("table#ledger tbody tr, table#journal tbody tr").forEach(row => row.onclick = () => {
+        let number = row.querySelector(".reference").textContent;
+        alert(`${number}`);
+
+        const transaction = journal.find(trans => trans.header.number == number);
+        one("#number").value = transaction.header.number;
+        one("#created").value = transaction.header.created;
+        one("#reference").value = transaction.header.reference;
+        one("#note").value = transaction.header.note;
+        one("#file").value = transaction.header.file;
+
+        const divs = all("fieldset#entries div");
+        let index = 0;
+        for (let div of divs) {
+            div.children[0].value = transaction.entries[index].entry;
+            div.children[1].value = transaction.entries[index].date;
+            div.children[2].value = transaction.entries[index].account;
+            div.children[3].value = transaction.entries[index].amount.toFixed(2);
+            ++index;
+        };
+        while (index < transaction.entries.length) {
+            const div = one("#entries").appendChild(one("#contra_entry_set").cloneNode(true));
+            const button = div.appendChild(document.createElement("button"));
+            button.textContent = "Remove";
+            button.setAttribute("type", "button");
+            button.setAttribute("onclick", "this.parentElement.remove();");
+
+            div.children[0].value = transaction.entries[index].entry;
+            div.children[1].value = transaction.entries[index].date;
+            div.children[2].value = transaction.entries[index].account;
+            div.children[3].value = transaction.entries[index].amount.toFixed(2);;
+            ++index;
+        }
+    });
 
     accout();
 
@@ -124,7 +181,7 @@ window.onload = () => {
     one("#transaction").onsubmit = event => {
 
         event.preventDefault();
-    
+
         const transaction = {
             header: {
                 number: one("#number").value,
@@ -136,13 +193,13 @@ window.onload = () => {
             entries: []
         };
 
-        if(transaction.header.number == 0)
-            transaction.header.number = journal.map(j => j.header.number).reduce((n1,n2) => n1 > n2 ? n1 : n2, 0) + 1;
+        if (transaction.header.number == 0)
+            transaction.header.number = journal.map(j => j.header.number).reduce((n1, n2) => n1 > n2 ? n1 : n2, 0) + 1;
 
         let credits = 0.00;
-        let debits = 0.00; 
+        let debits = 0.00;
         const divs = all("fieldset#entries div");
-        for(let div of divs) {
+        for (let div of divs) {
             const entry = {
                 entry: div.children[0].value,
                 date: div.children[1].value,
@@ -153,17 +210,17 @@ window.onload = () => {
             debits += entry.entry.localeCompare("debit") ? 0 : entry.amount;
             transaction.entries.push(entry);
         }
-    
+
         const update = one("#update").checked;
 
-        if(update == false && journal.some(existing => existing.header.number == transaction.header.number)) {
+        if (update == false && journal.some(existing => existing.header.number == transaction.header.number)) {
             alert(`Error: Transaction with number ${transaction.header.number} already exists`);
         }
-        else if(credits !== debits) {
+        else if (credits !== debits) {
             alert(`Error: The ${divs.length} entries for transaction ${transaction.header.reference} are not matching. Difference is ${credits - debits} €`);
         } else {
             alert(`Success: The ${divs.length} entries for transaction ${transaction.header.reference} are matching. Total credits/debits are ${credits} €`);
-            if(update) {
+            if (update) {
                 const index = journal.findIndex(existing => !transaction.header.reference.localeCompare(existing.header.reference));
                 journal[index] = transaction;
             } else {
@@ -171,14 +228,14 @@ window.onload = () => {
             }
             accout();
         }
-    }
+    };
 
     one("#transaction").onreset = () => {
         const divs = all("fieldset#entries :nth-child(1n+5)");
-        for(let div of divs) {
+        for (let div of divs) {
             div.remove();
         }
-    }
+    };
 
     one("#save").onclick = () => {
         const a = document.createElement('a');
@@ -186,8 +243,8 @@ window.onload = () => {
         const file = new Blob([content], { type: "application/javascript" });
         a.href = URL.createObjectURL(file);
         a.download = "test.js";
-        a.target="_blank"
+        a.target = "_blank"
         a.click();
         URL.revokeObjectURL(a.href);
     };
-}
+};
